96508a08200c9490312a20b8e0b1ab2deddc6272
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-03-10 22:49:46 +0100

    Init backend using spring initializr
    Two dependencies added: Spring Web and Spring Boot DevTools. Delete HELP.md. Add .gitignore in backend.

---
+++ backend/src/main/java/de/bund/digitalservice/knowthyselves/KnowThyselvesApplication.java
@@ -0,0 +1,13 @@
+package de.bund.digitalservice.knowthyselves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnowThyselvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnowThyselvesApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/de/bund/digitalservice/knowthyselves/KnowThyselvesApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bund.digitalservice.knowthyselves;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnowThyselvesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
