aad80eb4b432e6efeb8a759b8bc5392fe041ee53
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-03-11 13:31:30 +0100

    Copy miro script over here from OntoEngine repo
    Original location: https://github.com/benjaminaaron/OntoEngine/tree/main/scripts/miro-importer

---
+++ scripts/miro/README.md
@@ -0,0 +1,13 @@
+`config.json`:
+```json
+{
+  "ACCESS_TOKEN": "",
+  "BOARD_ID": "",
+  "EXPORT_DIR": "",
+  "BASE_URI": "",
+  "BASE_URI_PREFIX": "",
+  "SPARQL_ENDPOINT_UPDATE": ""
+}
+```
+
+`node script.js`
---
+++ scripts/miro/export.js
@@ -0,0 +1,174 @@
+/*const fs = require("fs")
+const path = require("path")
+// dependency --> "n3": "file:../../../N3.js",
+const N3 = require("n3"); // using this version that already has RDF-star support, built locally https://github.com/rdfjs/N3.js/pull/311
+const { namedNode, literal, quad } = DataFactory;
+const slugify = require("slugify")
+const { DataFactory } = N3;*/
+const config = require("./config.json")
+const { MiroApi } = require("@mirohq/miro-api")
+const { SparqlEndpointFetcher } = require("fetch-sparql-endpoint");
+const api = new MiroApi(config.ACCESS_TOKEN)
+const sparql = new SparqlEndpointFetcher();
+
+let board
+const nodes = {}
+const edges = []
+
+async function processEdge(edge) {
+  if (!(edge.startItem && edge.endItem && edge.captions)) return false
+  const startItem = await board.getItem(edge.startItem.id)
+  const endItem = await board.getItem(edge.endItem.id)
+  if (startItem.type === "sticky_note" &&
+      endItem.type === "sticky_note" &&
+      startItem.data.content &&
+      endItem.data.content
+  ) {
+    let edgeObj = {
+      from: edge.startItem.id,
+      to: edge.endItem.id,
+      label: undefined,
+      keyValuePairs: []
+    }
+    let edgeLabel = removeHtml(edge.captions[0].content)
+    let parts = edgeLabel.split(",")
+    if (parts.length > 1) {
+      edgeLabel = parts[0]
+      parts.slice(1).forEach(pair => edgeObj.keyValuePairs.push(pair.split(":")))
+    }
+    edgeObj.label = toCamelCase(edgeLabel, true)
+    edges.push(edgeObj)
+
+    let [fromIsLiteral, fromLabel] = processNodeLabel(startItem.data.content)
+    nodes[edge.startItem.id] = {
+      id: undefined,
+      label: fromLabel,
+      isLiteral: fromIsLiteral
+    }
+    let [toIsLiteral, toLabel] = processNodeLabel(endItem.data.content)
+    nodes[edge.endItem.id] = {
+      id: undefined,
+      label: toLabel,
+      isLiteral: toIsLiteral
+    }
+    console.log([fromLabel, edgeObj.label, toLabel])
+  }
+}
+
+const removeHtml = txt => {
+  return txt.replace(/<[^>]*>/g, "").trim()
+}
+
+const toCamelCase = (txt, isPredicate) => {
+  txt = txt.replace(/\s+(\w)/g, (match, letter) => letter.toUpperCase()).trim()
+  return isPredicate ? txt.charAt(0).toLowerCase() + txt.slice(1) : txt
+}
+
+function isValidDate(dateStr) {
+  const date = new Date(dateStr);
+  if (isNaN(date.getTime())) return false;
+  return date.toISOString().slice(0, 10) === dateStr; // YYYY-MM-DD
+}
+
+function isValidNumber(numberStr) {
+  numberStr = numberStr.replace(",", ".")
+  return !isNaN(numberStr) && !isNaN(parseFloat(numberStr))
+}
+
+const checkIfLiteral = txt => {
+  if (txt.startsWith("&#34;") && txt.endsWith("&#34;"))
+    return [true, "\"" + txt.substring(5, txt.length - 5) + "\""]
+  if (txt.startsWith("\"") && txt.endsWith("\""))
+    return [true, txt]
+  if (isValidDate(txt))
+    return [true, "\"" + txt + "\"^^xsd:date"]
+  if (isValidNumber(txt))
+    return [true, "\"" + txt + "\"^^xsd:decimal"]
+  return [false, txt]
+}
+
+const processNodeLabel = txt => {
+  txt = removeHtml(txt)
+  let [isLiteral, literalTxt] = checkIfLiteral(txt)
+  if (isLiteral) return [true, literalTxt]
+  return [false, toCamelCase(txt, false)]
+}
+
+const uri = localName => {
+  return config.BASE_URI + localName
+}
+
+(async function () {
+  board = await api.getBoard(config.BOARD_ID)
+
+  let query = "DELETE { ?s ?p ?o . } WHERE { ?s ?p ?o . }"
+  await sparql.fetchUpdate(config.SPARQL_ENDPOINT_UPDATE, query)
+
+  // let tgf = ""
+  let counter = 0
+  let triples = []
+  let quads = []
+
+  for await (const edge of board.getAllConnectors()) {
+    await processEdge(edge)
+  }
+
+  Object.entries(nodes).forEach(([id, node]) => {
+    node.id = ++ counter
+    // tgf += node.id + " " + node.label + "\n"
+  })
+
+  // tgf += "#\n"
+
+  for (const edge of edges) {
+    let from = nodes[edge.from]
+    let to = nodes[edge.to]
+    // tgf += from.id + " " + to.id + " " + edge.label + "\n"
+    triples.push([from.label, edge.label, to.label])
+
+    let subUri = uri(from.label)
+    let predUri = uri(edge.label)
+    // let triple = quad(namedNode(subUri), namedNode(predUri), to.isLiteral ? literal(to.label) : namedNode(uri(to.label)))
+    // quads.push(triple)
+
+    let objSparql = to.isLiteral ? to.label : ("<" + uri(to.label) + ">")
+    let statementStr = "<" + subUri + "> <" + predUri + "> " + objSparql;
+    query = "INSERT DATA { " + statementStr + " . }"
+    await sparql.fetchUpdate(config.SPARQL_ENDPOINT_UPDATE, query)
+
+    for (const pair of edge.keyValuePairs) { // RDF-star: statements about statements
+      let pred = toCamelCase(pair[0], true)
+      let [objIsLiteral, objLabel] = processNodeLabel(pair[1])
+      let tripleArr = ["<<" + from.label + " " + edge.label + " " + to.label + ">>", pred, objLabel]
+      triples.push(tripleArr)
+      console.log(tripleArr)
+
+      predUri = uri(pred)
+      // triple = quad(triple, namedNode(predUri), objIsLiteral ? literal(objLabel) : namedNode(uri(objLabel)))
+      // quads.push(triple)
+
+      objSparql = objIsLiteral ? objLabel : ("<" + uri(objLabel) + ">")
+      let rdfStarStatementStr = "<<" + statementStr + ">> <" + predUri + "> " + objSparql;
+      query = "INSERT DATA { " + rdfStarStatementStr + " . }"
+      await sparql.fetchUpdate(config.SPARQL_ENDPOINT_UPDATE, query)
+    }
+  }
+
+  // console.log(triples)
+/*const filename = "miro_" + slugify(board.name) + "_" + getTimestamp() + ".ttl"
+  const writer = new N3.Writer({ prefixes: { [config.BASE_URI_PREFIX]: config.BASE_URI } });
+  quads.forEach(quad => writer.addQuad(quad))
+  writer.end((error, result) => {
+    if (error) { console.error(error); return; }
+    console.log(result);
+    fs.mkdir(path.join(__dirname, config.EXPORT_DIR), () => {})
+    fs.writeFile(path.join(__dirname, config.EXPORT_DIR + "/" + filename), result, () => {})
+  });*/
+})()
+
+function getTimestamp() {
+  const pad = num => String(num).padStart(2, "0")
+  let date = new Date();
+  return date.getFullYear() + pad(date.getMonth() + 1) + pad(date.getDate())
+      + "-" +  pad(date.getHours()) + pad(date.getMinutes()) + pad(date.getSeconds());
+}
