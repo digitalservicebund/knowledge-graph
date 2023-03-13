011c844ce34253068e203bd3d59058832a8aa880
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-03-13 22:34:09 +0100

    Link all templates on main page

--- frontend/src/view/Main.jsx
+++ frontend/src/view/Main.jsx
@@ -1,8 +1,16 @@
+import { queryTemplates } from "../data/query-templates";
+import { Link } from "react-router-dom";
+
 function Main() {
 
   return (
     <div>
-      Main page
+      <strong>Templates:</strong>
+      { queryTemplates.map(t =>
+          <div key={t.id}>
+            <Link to={"/template/" + t.id}>{t.title}</Link>
+          </div>
+      )}
     </div>
   );
 }
