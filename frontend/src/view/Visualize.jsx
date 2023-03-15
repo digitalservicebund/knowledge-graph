1f21d2158670c9d52d9262fd066f4b49afa88242
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-03-15 22:45:45 +0100

    Wip: fetch triples for visu

--- frontend/src/view/Visualize.jsx
+++ frontend/src/view/Visualize.jsx
@@ -1,25 +1,50 @@
 import ForceGraph2D from "react-force-graph-2d";
+import { useEffect, useRef, useState } from "react";
+import { SparqlEndpointFetcher } from "fetch-sparql-endpoint";
+import config from "../config.json";
+const sparql = new SparqlEndpointFetcher()
 
 function Visualize() {
-  const graphData = {
-    nodes: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }],
-    links: [
-      { source: 1, target: 2 },
-      { source: 1, target: 3 },
-      { source: 1, target: 4 },
-    ]
-  };
+  const init = useRef(false);
+  const [graphData, setGraphData] = useState();
+
+  useEffect(() => {
+    if (!init.current) {
+      init.current = true
+      fetchTriples().then(() => {})
+    }
+  }, [])
+
+  async function fetchTriples() {
+    const query = "SELECT ?s ?p ?o WHERE { ?s ?p ?o . } LIMIT 3" // TODO support Quad
+    const bindingsStream = await sparql.fetchBindings(config.SPARQL_ENDPOINT, query)
+    const nodes = {}
+    const edges = []
+    bindingsStream.on("data", resultRow => {
+      const s = resultRow.s.value
+      const o = resultRow.o.value
+      if (!nodes[s]) nodes[s] = { id: s, label: s.split("#").pop() }
+      if (!nodes[o]) nodes[o] = { id: o, label: o.split("#").pop() }
+      edges.push({ source: s, target: o })
+    })
+    bindingsStream.on("end", () => {
+      setGraphData({ nodes: Object.values(nodes), links: edges })
+    })
+  }
 
   return (
       <div>
         <br/>
         <h2>Visualize</h2>
-        <ForceGraph2D
+        {graphData && <ForceGraph2D
+            width={1000}
+            height={600}
+            backgroundColor={"#eee"}
             graphData={graphData}
-            nodeLabel={"id"}
+            nodeLabel="label"
             linkDirectionalArrowLength={5}
             linkDirectionalArrowRelPos={1}
-        />
+        />}
       </div>
   );
 }
