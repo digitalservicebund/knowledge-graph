aae13ccb5d2260807c73f728f28120c8a7ded191
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-03-15 12:25:27 +0100

    Add an image of Katy to main page

--- frontend/src/view/Main.jsx
+++ frontend/src/view/Main.jsx
@@ -2,7 +2,13 @@ function Main() {
   return (
     <div>
       <br/>
-      <h2>Welcome to KnowThyselves!</h2>
+      <h1 style={{fontFamily: "monospace", color: "darkblue"}}>Hi, I am Katy.<br/>Welcome to KnowThyselves!</h1>
+      <img
+          src="https://user-images.githubusercontent.com/5141792/225292599-c8ae735f-10f9-42c3-89c2-267a1d60b790.png"
+          width="460"
+          alt="Katy"
+          title="Midjourney prompt: An old friendly lady in Steampunk look. Cables coming from her jacket connect to her glasses and go into her hair. Lots of buttons and LED lights on her. An old classic library in the background with stylish neon light elements. Photorealistic 3D render. (Feb 25, 2023, 3rd picture of variation 4)"
+      />
     </div>
   );
 }
