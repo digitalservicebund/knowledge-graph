236e0912a41bd8a1f0b10c6044a562c379521fc3
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-03-15 22:24:07 +0100

    Add basic ForceGraph to the Visualize page

--- frontend/src/view/Visualize.jsx
+++ frontend/src/view/Visualize.jsx
@@ -1,8 +1,25 @@
+import ForceGraph2D from "react-force-graph-2d";
+
 function Visualize() {
+  const graphData = {
+    nodes: [{ id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }],
+    links: [
+      { source: 1, target: 2 },
+      { source: 1, target: 3 },
+      { source: 1, target: 4 },
+    ]
+  };
+
   return (
       <div>
-        <br/><br/>
+        <br/>
         <h2>Visualize</h2>
+        <ForceGraph2D
+            graphData={graphData}
+            nodeLabel={"id"}
+            linkDirectionalArrowLength={5}
+            linkDirectionalArrowRelPos={1}
+        />
       </div>
   );
 }
