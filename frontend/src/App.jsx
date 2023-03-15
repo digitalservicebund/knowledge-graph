449e585bf11429cac601bb5db0f363aa4b3a12f8
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-03-15 11:03:07 +0100

    Adapt ResponsiveAppBar mui-example as TopBar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,18 +3,22 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Main from "./view/Main";
 import Add from "./view/Add";
 import Template from "./view/Template";
+import TopBar from "./component/TopBar";
 
 function App() {
   return (
-      <div style={{display: "flex",  justifyContent: "center", alignItems: "center"}}>
-        <BrowserRouter>
-          <Routes>
-            <Route path="/" element={<Main />}/>
-            <Route path="/add" element={<Add />}/>
-            <Route path="/template/:id" element={<Template />}  />
-          </Routes>
-        </BrowserRouter>
-      </div>
+      <>
+        <TopBar />
+        <div style={{display: "flex",  justifyContent: "center", alignItems: "center"}}>
+          <BrowserRouter>
+            <Routes>
+              <Route path="/" element={<Main />}/>
+              <Route path="/add" element={<Add />}/>
+              <Route path="/template/:id" element={<Template />}  />
+            </Routes>
+          </BrowserRouter>
+        </div>
+      </>
   );
 }
 
