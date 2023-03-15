c772e65d14e8a2aedc866d3b20b28d0b72a1546d
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-03-15 12:44:07 +0100

    Add save as template button to query as TODO

--- frontend/src/view/Query.jsx
+++ frontend/src/view/Query.jsx
@@ -1,8 +1,13 @@
+import Button from "@mui/material/Button";
+
 function Query() {
   return (
       <div>
         <br/><br/>
         <h2>Query</h2>
+        <Button variant="contained">
+          Save as template
+        </Button>
       </div>
   );
 }
