ba8845a8d3bd4b2d595a97bcc43d1edac7954e69
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-03-15 12:41:57 +0100

    Add the import/export wishlist :)

--- frontend/src/view/Data.jsx
+++ frontend/src/view/Data.jsx
@@ -1,6 +1,10 @@
 import { useState } from "react";
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
 import { SparqlEndpointFetcher } from "fetch-sparql-endpoint";
 import config from "../config.json";
 
@@ -16,6 +20,8 @@ function Data() {
   const handlePredChange = e => { setPred(e.target.value) };
   const handleObjChange = e => { setObj(e.target.value) };
 
+  const [io, setIO] = useState("import");
+
   function uri(localName) {
     return DEFAULT_NAMESPACE_PREFIX + ":" + localName;
   }
@@ -27,11 +33,14 @@ function Data() {
     await sparql.fetchUpdate(config.SPARQL_ENDPOINT, query);
   }
 
+  function handleRadioChange(e) {
+    setIO(e.target.value);
+  }
+
   return (
       <div>
         <br/><br/>
-        <h2>Add triples manually</h2>
-        <br/>
+        <p><strong>Add triples manually</strong></p>
         <TextField label="Subject" variant="outlined" value={sub} onChange={handleSubChange} />
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         <TextField label="Predicate" variant="outlined" value={pred} onChange={handlePredChange} />
@@ -40,6 +49,24 @@ function Data() {
         <Button style={{margin: "10px 0 0 25px"}} variant="contained" onClick={addTriple}>
           Submit
         </Button>
+        <br/><br/><br/>
+        <FormControl>
+          <RadioGroup row defaultValue="import" name="row-radio-buttons-group" onChange={handleRadioChange}>
+            <FormControlLabel value="import" control={<Radio />} label={<strong>Import from</strong>} />
+            <FormControlLabel value="export" control={<Radio />} label={<strong>Export to</strong>} />
+          </RadioGroup>
+        </FormControl>
+        <ul>
+          <li style={{textDecoration: "underline"}}>RDF/Turtle</li>
+          <li style={{textDecoration: "underline"}}>TGF</li>
+          <li style={{textDecoration: "underline"}}>SPARQL endpoint</li>
+          <li style={{textDecoration: "underline"}}>Markdown</li>
+          <li style={{textDecoration: "underline"}}>GraphML</li>
+          <li style={{textDecoration: "underline"}}>CSV</li>
+          <li style={{textDecoration: "underline"}}>Excel</li>
+          <li style={{textDecoration: "underline"}}>Google Spreadsheet</li>
+          <li style={{textDecoration: "underline"}}>Confluence page</li>
+        </ul>
       </div>
   );
 }
