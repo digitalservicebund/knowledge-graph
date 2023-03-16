3e4d3730e66bf26193dd6f9b74bb184c9269e285
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-03-16 19:29:12 +0100

    Improve styling of templates

--- frontend/src/view/Templates.jsx
+++ frontend/src/view/Templates.jsx
@@ -1,18 +1,52 @@
 import { queryTemplates } from "../data/query-templates";
-import { Link } from "react-router-dom";
+import Box from "@mui/material/Box";
+import Paper from "@mui/material/Paper";
+import { useNavigate } from "react-router-dom";
 
 function Templates() {
+  const navigate = useNavigate();
+
+  const paperStyle = {
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    flexDirection: "column",
+    backgroundColor: "rgb(0, 0, 255, 0.08)",
+    cursor: "pointer"
+  }
+
+  const descStyle = {
+    textAlign: "center",
+    padding: "10px",
+    fontSize: "small"
+  }
+
+  const handlePaperClick = (id) => {
+    navigate("/template/" + id)
+  }
+
   return (
       <div>
-        <br/><br/>
+        <br/>
         <h2>Templates</h2>
-        <ul>
+        <Box
+            sx={{
+              display: "flex",
+              flexWrap: "wrap",
+              "& > :not(style)": {
+                m: 1,
+                width: 200,
+                height: 120,
+              },
+            }}
+        >
           { queryTemplates.map(t =>
-              <li key={t.id}>
-                <Link to={"/template/" + t.id}>{t.title}</Link>
-              </li>
+              <Paper style={paperStyle} key={t.id} elevation={4} onClick={() => handlePaperClick(t.id)}>
+                <strong>{t.title}</strong>
+                <div style={descStyle}>{t.description}</div>
+              </Paper>
           )}
-        </ul>
+        </Box>
       </div>
   );
 }
