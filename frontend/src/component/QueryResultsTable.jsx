5e34450a509cffbfac9f66bf115782f6a5c9d7c0
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-03-18 21:19:15 +0100

    Extract to new component QueryResultsTable

---
+++ frontend/src/component/QueryResultsTable.jsx
@@ -0,0 +1,88 @@
+import { useEffect, useRef, useState } from "react";
+import TableContainer from "@mui/material/TableContainer";
+import Table from "@mui/material/Table";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import TableCell from "@mui/material/TableCell";
+import TableBody from "@mui/material/TableBody";
+import OpenInNewIcon from "@mui/icons-material/OpenInNew";
+import config from "../config.json";
+import { SparqlEndpointFetcher } from "fetch-sparql-endpoint";
+const sparql = new SparqlEndpointFetcher()
+
+function QueryResultsTable(props) {
+
+  const init = useRef(false);
+  const [resultData, setResultData] = useState();
+
+  useEffect(() => {
+    if (init.current) return
+    init.current = true;
+    runQuery().then(() => {})
+  }, [])
+
+  async function runQuery() {
+    let variables = []
+    let rows = []
+    const bindingsStream = await sparql.fetchBindings(config.SPARQL_ENDPOINT + "/demo", props.query)
+    bindingsStream.on("variables", vars =>
+        variables = vars.map(v => v.value)
+    )
+    bindingsStream.on("data", resultRow => {
+      console.log(resultRow)
+      rows.push(resultRow)
+    })
+    bindingsStream.on("end", () => {
+      setResultData({
+        variables: variables,
+        rows: rows
+      })
+    })
+  }
+
+  function buildCellContent(col, variable) {
+    if (!col) return
+    variable = variable.toLowerCase()
+    if (col.termType === "NamedNode") {
+      return <span title={col.value}>{col.value.split("#").pop()}</span>
+    }
+    // literal
+    if (variable.includes("imageurl")) {
+      return <img src={col.value} title={col.value} width="120" alt="logo"/>
+    }
+    if (variable.includes("url")) {
+      return <>
+        <a href={col.value} title={col.value} target="_blank" rel="noreferrer">link</a>
+        <OpenInNewIcon style={{fontSize: "large", verticalAlign: "middle", marginLeft: "2px"}}/>
+      </>
+    }
+    return <span style={{color:"green"}}>{col.value}</span>
+  }
+
+  return (
+      <div>
+        {resultData &&
+          <TableContainer>
+            <Table sx={{width: 600}}>
+              <TableHead>
+                <TableRow>
+                  {resultData.variables.map(h => <TableCell align="right" key={h}>{h}</TableCell>)}
+                </TableRow>
+              </TableHead>
+              <TableBody>
+                {resultData.rows.map((col, idx) => (
+                    <TableRow key={idx} sx={{"&:last-child td, &:last-child th": {border: 0}}}>
+                      {resultData.variables.map((h, idx) =>
+                          <TableCell align="right" key={idx}>{buildCellContent(col[h], h)}</TableCell>
+                      )}
+                    </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        }
+    </div>
+  );
+}
+
+export default QueryResultsTable;
--- frontend/src/view/Template.jsx
+++ frontend/src/view/Template.jsx
@@ -1,28 +1,16 @@
 import { useParams } from "react-router-dom";
 import { useEffect, useRef, useState } from "react";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import { SparqlEndpointFetcher } from "fetch-sparql-endpoint";
 import { queryTemplates } from "../data/query-templates";
-import config from "../config.json";
 import Button from "@mui/material/Button";
 import TextField from "@mui/material/TextField";
-import OpenInNewIcon from "@mui/icons-material/OpenInNew";
-import Chart from "chart.js/auto";
 import { Line } from "react-chartjs-2";
-
-const sparql = new SparqlEndpointFetcher()
+import QueryResultsTable from "../component/QueryResultsTable";
 
 function Template() {
   let { id } = useParams();
   const init = useRef(false);
   const [template, setTemplate] = useState({});
-  const [variables, setVariables] = useState([]);
-  const [rows, setRows] = useState([]);
+  const [query, setQuery] = useState();
   const [chartData, setChartData] = useState();
   /*{labels: ["Jan", "Feb", "Mar"],
     datasets: [{
@@ -37,36 +25,6 @@ function Template() {
     setTemplate(queryTemplates.find(t => t.id === id));
   }, [])
 
-  async function runQuery() {
-    const bindingsStream = await sparql.fetchBindings(config.SPARQL_ENDPOINT + "/demo", template.query)
-    bindingsStream.on("variables", vars =>
-      setVariables(vars.map(v => v.value))
-    )
-    bindingsStream.on("data", resultRow => {
-      console.log(resultRow)
-      setRows(rows => [...rows, resultRow])
-    })
-  }
-
-  function buildCellContent(col, variable) {
-    if (!col) return
-    variable = variable.toLowerCase()
-    if (col.termType === "NamedNode") {
-      return <span title={col.value}>{col.value.split("#").pop()}</span>
-    }
-    // literal
-    if (variable.includes("imageurl")) {
-      return <img src={col.value} title={col.value} width="120" alt="logo"/>
-    }
-    if (variable.includes("url")) {
-      return <>
-        <a href={col.value} title={col.value} target="_blank" rel="noreferrer">link</a>
-        <OpenInNewIcon style={{fontSize: "large", verticalAlign: "middle", marginLeft: "2px"}}/>
-      </>
-    }
-    return <span style={{color:"green"}}>{col.value}</span>
-  }
-
   return (
       <div style={{textAlign: "center"}}>
         { template &&
@@ -76,30 +34,13 @@ function Template() {
               { template.choices && template.choices.map(c => {
                 return <TextField key={c.label} label={c.label} variant="standard"/>
               })}
-              { variables.length === 0 &&
-                  <Button style={{margin: "20px"}} variant="contained" onClick={runQuery}>
+              {!query &&
+                  <Button style={{margin: "20px"}} variant="contained" onClick={() => setQuery(template.query)}>
                     Run query
                   </Button>
               }
               <br/>
-              <TableContainer>
-                <Table sx={{ width: 600 }}>
-                  <TableHead>
-                    <TableRow>
-                      { variables.map(h => <TableCell align="right" key={h}>{h}</TableCell>) }
-                    </TableRow>
-                  </TableHead>
-                  <TableBody>
-                    {rows.map((col, idx) => (
-                        <TableRow key={idx} sx={{ "&:last-child td, &:last-child th": { border: 0 } }}>
-                          { variables.map((h, idx) =>
-                              <TableCell align="right" key={idx}>{buildCellContent(col[h], h)}</TableCell>
-                          )}
-                        </TableRow>
-                    ))}
-                  </TableBody>
-                </Table>
-              </TableContainer>
+              {query && <QueryResultsTable query={query}/>}
             </>
         }
         { !template && "No template with id " + id + " found" }
