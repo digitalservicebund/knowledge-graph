a783e8928eb546d2f19239f1fad342c933d8e43f
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-04-11 15:23:45 +0200

    Rename to DatasetManagementController

--- backend/src/main/java/de/bund/digitalservice/knowthyselves/controller/DatasetManagementController.java
+++ backend/src/main/java/de/bund/digitalservice/knowthyselves/controller/DatasetManagementController.java
@@ -12,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/knowthyselves/ds")
 @CrossOrigin(origins = "http://localhost:3000")
-public class DatasetController {
-  private final Logger logger = LogManager.getLogger(DatasetController.class);
+public class DatasetManagementController {
+  private final Logger logger = LogManager.getLogger(DatasetManagementController.class);
 
   @PostMapping(value = "/new")
   public String newDataset(@RequestBody Map<String, String> request) {
