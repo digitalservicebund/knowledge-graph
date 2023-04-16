9c44af129af044b7b337948ed55ee6bb9fddbc63
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-04-16 17:10:34 +0200

    Make runQuery return a ResponseEntity too

--- backend/src/main/java/de/bund/digitalservice/knowthyselves/controller/QueryController.java
+++ backend/src/main/java/de/bund/digitalservice/knowthyselves/controller/QueryController.java
@@ -4,6 +4,7 @@ import de.bund.digitalservice.knowthyselves.QueryService;
 import java.util.Map;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -23,8 +24,7 @@ public class QueryController {
   }
 
   @PostMapping(value = "")
-  public String runQuery(@RequestBody Map<String, String> request) {
-    String query = request.get("query");
-    return queryService.runQuery(query);
+  public ResponseEntity<String> runQuery(@RequestBody Map<String, String> request) {
+    return ResponseEntity.ok(queryService.runQuery(request.get("query")));
   }
 }
