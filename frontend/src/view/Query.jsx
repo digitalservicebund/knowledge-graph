8a3608e77f0105603a9308c693ae8b6690b45bd4
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-04-18 16:49:28 +0200

    Implement saving of template in meta dataset

--- frontend/src/view/Query.jsx
+++ frontend/src/view/Query.jsx
@@ -4,6 +4,7 @@ import Button from "@mui/material/Button";
 import Yasgui from "@triply/yasgui";
 import "@triply/yasgui/build/yasgui.min.css";
 import QueryResultsTable from "../component/QueryResultsTable";
+import slugify from "slugify";
 
 function Query() {
   const init = useRef(false);
@@ -29,6 +30,36 @@ function Query() {
 
   async function runQuery() {
     setQuery(yasgui.current.getTab().getQuery())
+  }
+
+  function saveAsTemplate() {
+    let title = prompt("Enter the title of your template")
+    title = title.trim()
+    if (!title) {
+      alert("Template must have a title")
+      return
+    }
+    let id = slugify(title)
+    let description = prompt("Enter an optional description")
+    description = description.trim()
+    let query = yasgui.current.getTab().getQuery()
+    let insertQuery = "PREFIX : <https://digitalservice.bund.de/kg#>\n" +
+        "INSERT DATA {\n" +
+        "    :" + id + " :isA :QueryTemplate ;\n" +
+        "    :hasTitle \"" + title + "\" ;\n" +
+        (description ? "    :hasDescription \"" + description + "\" ;\n" : "") +
+        "    :hasQuery \"\"\"\n" + query + "\n\"\"\" .\n" +
+        "}"
+    // let runOnDatasets TODO
+    fetch("http://localhost:8080/api/v1/knowthyselves/query/insert", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ query: insertQuery, dataset: "meta" })
+    })
+    .then(response => response.text())
+    .then(responseText => {
+      console.log("Response:", responseText)
+    })
   }
 
   return (
@@ -65,7 +96,7 @@ function Query() {
           Run query
         </Button>
         &nbsp;&nbsp;&nbsp;&nbsp;
-        <Button variant="outlined">
+        <Button variant="outlined" onClick={saveAsTemplate}>
           Save as template
         </Button>
         <br/><br/>
