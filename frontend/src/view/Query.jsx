f4784ff467c70e72dfc05cf4b95960c0d1a2772c
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-04-21 11:15:57 +0200

    Implement save-template dialog

--- frontend/src/view/Query.jsx
+++ frontend/src/view/Query.jsx
@@ -6,17 +6,31 @@ import "@triply/yasgui/build/yasgui.min.css";
 import QueryResultsTable from "../component/QueryResultsTable";
 import slugify from "slugify";
 import { datasetNamesToOneString, fetchInsert, fetchSelect } from "../utils";
+import TextField from "@mui/material/TextField";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
+import Chip from "@mui/material/Chip";
+import Autocomplete from "@mui/material/Autocomplete";
 
 function Query() {
-  const init = useRef(false);
-  const yasgui = useRef();
-  const [datasets, setDatasets] = useState({ main: true, meta: false });
-  const [queryResultData, setQueryResultData] = useState();
-
-  const handleCheckboxChange = (event) => {
-    const { name, checked } = event.target;
-    setDatasets({ ...datasets, [name]: checked });
-  };
+  const init = useRef(false)
+  const yasgui = useRef()
+  const tags = useRef(["experimental"])
+  const [datasets, setDatasets] = useState({ main: true, meta: false })
+  const [queryResultData, setQueryResultData] = useState()
+  const [dialogOpen, setDialogOpen] = useState(false)
+  const [formValues, setFormValues] = useState({
+    title: "",
+    description: "",
+    main: true,
+    meta: false,
+    tags: []
+  })
 
   useEffect(() => {
     if (init.current) return
@@ -26,8 +40,63 @@ function Query() {
         "PREFIX : <https://digitalservice.bund.de/kg#>\n"
         + "SELECT * WHERE { \n  ?subject ?predicate ?object .\n}"
     );
-    return () => {};
   }, []);
+
+  const handleDatasetChange = (event) => {
+    const { name, checked } = event.target
+    setDatasets({ ...datasets, [name]: checked })
+    setFormValues({ ...formValues, [name]: checked })
+  }
+
+  const handleDialogFormChange = (event, tags) => {
+    if (tags) {
+      tags = tags.map((tag) => (tag.startsWith("Add new tag ") ? tag.slice(13, -1) : tag))
+      setFormValues({ ...formValues, tags: tags })
+      return
+    }
+    const { name, value, checked, type } = event.target
+    setFormValues({ ...formValues, [name]: type === "checkbox" ? checked : value })
+  }
+
+  const handleSave = () => {
+    let title = formValues.title.trim()
+    if (!title) {
+      alert("Template must have a title")
+      return
+    }
+    let mainDsSelected = formValues.main
+    let metaDsSelected = formValues.meta
+    if (!mainDsSelected && !metaDsSelected) {
+      alert("At least one dataset must be selected")
+      return
+    }
+    let id = slugify(title)
+    let description = formValues.description.trim()
+    let query = yasgui.current.getTab().getQuery()
+
+    let insertQuery = "PREFIX : <https://digitalservice.bund.de/kg#> " +
+        "INSERT DATA { " +
+        "    :" + id + " :isA :QueryTemplate ; " +
+        "    :hasTitle \"" + title + "\" ; " +
+        (description ? "    :hasDescription \"" + description + "\" ; " : "") +
+        (mainDsSelected ? "    :runsOnDataset \"main\" ; " : "") +
+        (metaDsSelected ? "    :runsOnDataset \"meta\" ; " : "") +
+        formValues.tags.map((tag) => "    :hasTag \"" + tag + "\" ; ").join(" ") +
+        "    :hasQuery \"\"\"" + query + "\n\"\"\" . " +
+        "}"
+    fetchInsert(insertQuery, "meta", responseText => {
+      console.log("Response:", responseText)
+      setDialogOpen(false)
+    })
+  }
+
+  const handleFilterOptions = (options, params) => {
+    const filtered = options.filter((option) =>
+        option.toLowerCase().includes(params.inputValue.toLowerCase()))
+    if (params.inputValue !== "" && !options.includes(params.inputValue))
+      filtered.push("Add new tag \"" + params.inputValue + "\"")
+    return filtered
+  }
 
   async function runQuery() {
     let ds = datasetNamesToOneString(datasets)
@@ -45,41 +114,19 @@ function Query() {
     })
   }
 
-  function saveAsTemplate() {
-    let title = prompt("Enter the title of your template")
-    title = title.trim()
-    if (!title) {
-      alert("Template must have a title")
-      return
-    }
-    let id = slugify(title)
-    let description = prompt("Enter an optional description")
-    description = description.trim()
-    let query = yasgui.current.getTab().getQuery()
-    let insertQuery = "PREFIX : <https://digitalservice.bund.de/kg#> " +
-        "INSERT DATA { " +
-        "    :" + id + " :isA :QueryTemplate ; " +
-        "    :hasTitle \"" + title + "\" ; " +
-        (description ? "    :hasDescription \"" + description + "\" ; " : "") +
-        "    :hasQuery \"\"\"" + query + "\n\"\"\" . " +
-        "}"
-    // let runOnDatasets TODO
-    fetchInsert(insertQuery, "meta", responseText => console.log("Response:", responseText))
-  }
-
   return (
       <div>
         <h2>Query</h2>
         <div id="yasgui" style={{width: "700px"}} />
         <br/>
         <div style={{paddingBottom: "20px", color: "gray"}}>
-          Datasets:
+          Datasets to run this on:
           <label>
             <input
                 type="checkbox"
                 name="main"
                 checked={datasets.main}
-                onChange={handleCheckboxChange}
+                onChange={handleDatasetChange}
             />
             main
           </label>
@@ -88,7 +135,7 @@ function Query() {
                 type="checkbox"
                 name="meta"
                 checked={datasets.meta}
-                onChange={handleCheckboxChange}
+                onChange={handleDatasetChange}
             />
             meta
           </label>
@@ -97,9 +144,87 @@ function Query() {
           Run query
         </Button>
         &nbsp;&nbsp;&nbsp;&nbsp;
-        <Button variant="outlined" onClick={saveAsTemplate}>
+        <Button variant="outlined" onClick={() => setDialogOpen(true)}>
           Save as template
         </Button>
+        <Dialog open={dialogOpen} onClose={() => setDialogOpen(false)}>
+          <DialogTitle>Save as template</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              By saving this query as a template you make it accessible to others.<br/>
+              If you are not sure others will find it useful but you want to
+              try things out (please do!), you can add the tag <i>experimental</i>.
+            </DialogContentText>
+            <br/>
+            <TextField
+                label="Title"
+                name="title"
+                fullWidth
+                variant="standard"
+                value={formValues.title}
+                onChange={handleDialogFormChange}
+            />
+            <br/><br/>
+            <TextField
+                label="Description (optional)"
+                name="description"
+                fullWidth
+                variant="standard"
+                value={formValues.description}
+                onChange={handleDialogFormChange}
+            />
+            <br/><br/><br/>
+            <DialogContentText>Datasets to run this on:</DialogContentText>
+            <FormControlLabel
+                control={
+                  <Checkbox
+                      disabled
+                      name="main"
+                      checked={formValues.main}
+                      onChange={handleDialogFormChange}
+                  />
+                }
+                label="Main"
+            />
+            <FormControlLabel
+                control={
+                  <Checkbox
+                      disabled
+                      name="meta"
+                      checked={formValues.meta}
+                      onChange={handleDialogFormChange}
+                  />
+                }
+                label="Meta"
+            />
+            <br/><br/><br/>
+            <Autocomplete
+                multiple
+                value={formValues.tags}
+                onChange={handleDialogFormChange}
+                freeSolo
+                options={tags.current}
+                filterOptions={handleFilterOptions}
+                renderTags={(value, getTagProps) =>
+                    value.map((option, index) => (
+                        <Chip
+                            variant="outlined"
+                            label={option}
+                            {...getTagProps({ index })}
+                            sx={{ backgroundColor: "lightgoldenrodyellow" }}
+                        />
+                    ))
+                }
+                renderInput={(params) => (
+                    <TextField {...params} label="Tags" placeholder="Type or select" />
+                )}
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={() => setDialogOpen(false)}>Cancel</Button>
+            <Button onClick={handleSave}>Save</Button>
+          </DialogActions>
+        </Dialog>
         <br/><br/>
         <QueryResultsTable queryResultData={queryResultData} />
       </div>
