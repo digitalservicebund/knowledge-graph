2c41e7d7f7935158b4d41d6a9a58e2b912780bc6
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-04-27 14:59:29 +0200

    Recursive to support nested RDF-star triples

--- frontend/src/component/QueryResultsTable.jsx
+++ frontend/src/component/QueryResultsTable.jsx
@@ -9,32 +9,34 @@ import fileDownload from "js-file-download";
 
 function QueryResultsTable(props) {
 
-  function buildCellContent(col, variable, rowIdx) {
+  function buildCellContent(entity, variable) {
+    if (!entity) return
     variable = variable.toLowerCase()
-    if (!col) return
-    if (col.type === "uri") {
-      return <span title={col.value}>{col.value.split("#").pop()}</span>
-    }
     // RDF-star
-    if (col.type === "triple") {
-      let triple = col.value
-      let rdfStarTripleFull = "<<" + triple.subject.value + " " + triple.predicate.value + " " + triple.object.value + ">>"
-      let rdfStarTripleShort = "<<" + triple.subject.value.split("#").pop() + " "
-          + triple.predicate.value.split("#").pop() + " "
-          + (triple.object.type === "uri" ? triple.object.value.split("#").pop() : triple.object.value) + ">>"
-      return <span title={rdfStarTripleFull}>{rdfStarTripleShort}</span>
+    if (entity.type === "triple") {
+      return <span title="Triple with RDF-star syntax">
+        <span>{"<<"}</span>
+        {buildCellContent(entity.value.subject, variable)}&nbsp;
+        {buildCellContent(entity.value.predicate, variable)}&nbsp;
+        {buildCellContent(entity.value.object, variable)}
+        <span>{">>"}</span>
+      </span>
+    }
+    // uri
+    if (entity.type === "uri") {
+      return <span title={entity.value}>{entity.value.split("#").pop()}</span>
     }
     // literal
     if (variable.includes("imageurl")) {
-      return <img src={col.value} title={col.value} width="60" alt="picture"/>
+      return <img src={entity.value} title={entity.value} width="60" alt="picture"/>
     }
     if (variable.includes("url")) {
       return <>
-        <a href={col.value} title={col.value} target="_blank" rel="noreferrer">{variable.split("url")[0]}</a>
+        <a href={entity.value} title={entity.value} target="_blank" rel="noreferrer">{variable.split("url")[0]}</a>
         <OpenInNewIcon style={{fontSize: "large", verticalAlign: "middle", marginLeft: "2px"}}/>
       </>
     }
-    return <span style={{color:"green"}}>{col.value === "0" ? "" : col.value}</span>
+    return <span style={{color:"green"}}>{entity.value}</span>
   }
 
   function download(type) {
@@ -71,11 +73,11 @@ function QueryResultsTable(props) {
                 </TableRow>
               </TableHead>
               <TableBody>
-                {props.queryResultData.rows.map((col, rowIdx) => (
+                {props.queryResultData.rows.map((row, rowIdx) => (
                     <TableRow key={rowIdx} sx={{"&:last-child td, &:last-child th": {border: 0}}}>
                       <TableCell align="left"><span style={{color: "gray", fontSize: "small"}}>{rowIdx + 1}</span></TableCell>
-                      {props.queryResultData.variables.map((v, idx) =>
-                          <TableCell align="right" key={idx}>{buildCellContent(col[v], v, rowIdx)}</TableCell>
+                      {props.queryResultData.variables.map((variable, idx) =>
+                          <TableCell align="right" key={idx}>{buildCellContent(row[variable], variable)}</TableCell>
                       )}
                     </TableRow>
                 ))}
