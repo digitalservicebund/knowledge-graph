911fd0aeb37b7f3b9ee4a84ca862f025c9a78c97
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-04-27 15:56:39 +0200

    Add TaskService with one for moving triples

---
+++ backend/src/main/java/de/bund/digitalservice/knowthyselves/TaskService.java
@@ -0,0 +1,77 @@
+package de.bund.digitalservice.knowthyselves;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.TxnType;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.SimpleSelector;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TaskService {
+  private final Logger logger = LogManager.getLogger(TaskService.class);
+
+  private final DatasetService datasetService;
+
+  public TaskService(DatasetService datasetService) {
+    this.datasetService = datasetService;
+  }
+
+  public String moveTriplesWithPredicateToAnotherDataset(String from, String to, String predicate) {
+    logger.info("Moving triples with predicate {} from dataset {} to dataset {}", predicate, from, to);
+    Dataset fromDs = datasetService.getDataset(from);
+    Dataset toDs = datasetService.getDataset(to);
+
+    int count = 0;
+    String response = "";
+
+    fromDs.begin(TxnType.READ);
+    Model fromModel = fromDs.getDefaultModel();
+    List<Statement> matchingStatements = new ArrayList<>();
+    try {
+      matchingStatements = fromModel.listStatements(new SimpleSelector(
+          null, fromModel.createProperty(predicate), (RDFNode) null)).toList();
+      count = matchingStatements.size();
+    } catch (Exception e) {
+      response += " Error reading statements from dataset " + from + ": " + e.getMessage();
+    } finally {
+      fromDs.end();
+    }
+
+    toDs.begin(TxnType.WRITE);
+    Model toModel = toDs.getDefaultModel();
+    try {
+      toModel.add(matchingStatements);
+      toDs.commit();
+    } catch (Exception e) {
+      toDs.abort();
+      response += " Error writing statements to dataset " + to + ": " + e.getMessage();
+      logger.error(e);
+    } finally {
+      toDs.end();
+    }
+
+    fromDs.begin(TxnType.WRITE);
+    fromModel = fromDs.getDefaultModel();
+    try {
+      fromModel.remove(matchingStatements);
+      fromDs.commit();
+    } catch (Exception e) {
+      fromDs.abort();
+      response += " Error removing statements from dataset " + from + ": " + e.getMessage();
+      logger.error(e);
+    } finally {
+      fromDs.end();
+    }
+
+    return response.isEmpty() ?
+        ("Successfully moved " + count + " statements with the predicate " + predicate +
+        " from the dataset " + from + " to the dataset " + to)
+        : response;
+  }
+}
---
+++ backend/src/main/java/de/bund/digitalservice/knowthyselves/controller/TaskController.java
@@ -0,0 +1,33 @@
+package de.bund.digitalservice.knowthyselves.controller;
+
+import de.bund.digitalservice.knowthyselves.TaskService;
+import java.util.Map;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/v1/knowthyselves/task")
+@CrossOrigin(origins = "http://localhost:3000")
+public class TaskController {
+
+  private final TaskService taskService;
+
+  public TaskController(TaskService taskService) {
+    this.taskService = taskService;
+  }
+
+  @PostMapping(value = "")
+  public ResponseEntity<String> task(@RequestBody Map<String, String> request) {
+    String task = request.get("task");
+    if (task.equals("move-triples-with-predicate-to-another-dataset")) {
+      return ResponseEntity.ok(taskService.moveTriplesWithPredicateToAnotherDataset(
+          request.get("from"), request.get("to"), request.get("predicate")
+      ));
+    }
+    return ResponseEntity.internalServerError().body("Task unknown: " + task);
+  }
+}
