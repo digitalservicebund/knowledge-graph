5816b32e80b03dc17f47a71985f4276e30f377b2
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-05-25 22:44:02 +0200

    Implement stacked line chart for hiring timeline

--- frontend/src/component/LineChart.jsx
+++ frontend/src/component/LineChart.jsx
@@ -1,4 +1,4 @@
-import {useEffect, useRef, useState} from "react";
+import { useEffect, useRef, useState } from "react";
 import chartjs from "chart.js/auto"; // required for react-chartjs-2
 import { Line } from "react-chartjs-2";
 import fileDownload from "js-file-download";
@@ -15,30 +15,46 @@ function LineChart(props) {
 
   const computeDatasets = () => {
     let row = props.queryResultData.rows[0]
-    let labels = []
+    let dateLabels = []
     let datasets = {}
+
     props.queryResultData.variables.forEach(variable => {
       let date = variable.substring(0, 7)
-      if (!labels.includes(date)) labels.push(date)
+      if (!dateLabels.includes(date)) dateLabels.push(date)
       let discipline = variable.substring(7, variable.length)
-      if (!datasets[discipline]) datasets[discipline] = { label: discipline, data: [], tension: 0.2 } //, fill: "origin" }
+      if (!datasets[discipline]) datasets[discipline] =
+          { label: discipline, data: [], tension: 0.2, fill: "origin", borderWidth: 0, pointRadius: 0 }
       let value = Number(row[variable].value)
       datasets[discipline].data.push(value)
     })
-    // delete datasets["Total"]
-    setChartData({ labels: labels, datasets: Object.values(datasets) })
+
+    let sortedDatasetLabels = Object.values(datasets).sort((a, b) => {
+      let lastA = a.data[a.data.length - 1]
+      let lastB = b.data[b.data.length - 1]
+      return lastB - lastA
+    }).map(dataset => dataset.label)
+
+    let datapoints = new Array(dateLabels.length).fill(0)
+    let stackedDatasets = []
+    for (let datasetLabel of sortedDatasetLabels) {
+      datapoints = datapoints.map((value, idx) => value + datasets[datasetLabel].data[idx])
+      stackedDatasets.push(
+          { label: datasetLabel, data: datapoints, tension: 0.2, fill: "origin", borderWidth: 0, pointRadius: 0 })
+    }
+
+    setChartData({ labels: dateLabels, datasets: stackedDatasets })
   }
 
   const chartOptions = {
     animation: {
       onComplete: function(animation) {
-        /*let chartInstance = animation.chart
+        let chartInstance = animation.chart
         chartInstance.data.datasets.forEach(dataset => {
           if(dataset.backgroundColor) {
             dataset.backgroundColor = dataset.backgroundColor.replace(/[^,]+(?=\))/, "1")
           }
         })
-        chartInstance.update()*/
+        chartInstance.update()
       }
     }
   }
