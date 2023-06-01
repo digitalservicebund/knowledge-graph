8515100138c311473ba6481288eb35fcbeaa7480
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-06-01 10:16:48 +0200

    Prepare two modes for stakeholder demo use case

--- frontend/src/view/Experimental.jsx
+++ frontend/src/view/Experimental.jsx
@@ -1,8 +1,15 @@
 import Button from "@mui/material/Button";
 import { SparqlEndpointFetcher } from "fetch-sparql-endpoint";
+import TextField from "@mui/material/TextField";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Radio from "@mui/material/Radio";
+import {useState} from "react";
 const sparql = new SparqlEndpointFetcher()
 
 function Experimental() {
+  const [mode, setMode] = useState("person")
+  const [input, setInput] = useState("")
 
   async function dev() {
     const personName = "Annalena Baerbock"
@@ -29,9 +36,21 @@ function Experimental() {
 
   return (
       <div>
-        <h2>Experimental</h2>
-        <Button variant="contained" onClick={dev}>
-          dev
+        <br/>
+        <h2>Stakeholders</h2>
+        <RadioGroup row defaultValue="person" onChange={(e) => setMode(e.target.value)}>
+          <FormControlLabel value="person" control={<Radio />} label="What do we know about this Person?"/>
+          <FormControlLabel value="field" control={<Radio />} label="Who do we know in this field?"/>
+        </RadioGroup>
+        <br/><br/>
+        <TextField
+            label={mode === "person" ? "Name" : "Field"}
+            value={input}
+            style={{ width: "350px" }}
+            onChange={(e) => setInput(e.target.value)}
+        />
+        <Button variant="contained" onClick={dev} style={{margin: "9px 0 0 25px"}}>
+          Search
         </Button>
       </div>
   );
