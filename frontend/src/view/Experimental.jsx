c3b591eaeac923764e11551fd0bd2b8e9b01f96b
benjaminaaron <benjamin.degenhart@digitalservice.bund.de>
2023-06-04 12:31:11 +0200

    Implement rest of person search use case

--- frontend/src/view/Experimental.jsx
+++ frontend/src/view/Experimental.jsx
@@ -41,7 +41,7 @@ function Experimental() {
     }
 
     const statement = str => {
-        return <div style={{ color: "blue", marginLeft: "40px" }}>
+        return <div style={{ color: "blue", margin: "10px 10px 10px 40px" }}>
             {str}
         </div>
     }
@@ -68,7 +68,84 @@ function Experimental() {
                                 setTimeout(() => {
                                     appendOutput(activity("Checking the Knowledge Graph"))
                                     setTimeout(() => {
-                                        // TODO
+                                        query = `
+PREFIX : <https://digitalservice.bund.de/kg#>
+SELECT
+  (COUNT(DISTINCT ?t4gProj) AS ?Tech4GermanyProjects)
+  (COUNT(DISTINCT ?w4gProj) AS ?Work4GermanyProjects)
+  (COUNT(DISTINCT ?dsProj) AS ?DigitalServiceProjects)
+WHERE {
+  {
+    ?t4gProj :isA :FellowshipProject .
+    ?t4gProj :wasInFellowshipProgram :Tech4Germany .
+    ?t4gProj :hadParticipatingMinistry ?partner .
+  }
+  UNION
+  {
+    ?w4gProj :isA :FellowshipProject .
+    ?w4gProj :wasInFellowshipProgram :Work4Germany .
+    ?w4gProj :hadParticipatingMinistry ?partner .
+  }
+  UNION
+  {
+    ?dsProj :isA :DigitalServiceProject .
+    ?dsProj :hasClient ?partner .
+  }
+  ?partner :hasName "${office}" .
+} GROUP BY ?partner`
+                                        fetchSelect(query, "main", responseJson => {
+                                            console.log("Response:", responseJson)
+                                            let row = responseJson.results.bindings[0]
+                                            let t4g = Number(row["Tech4GermanyProjects"].value)
+                                            let w4g = Number(row["Work4GermanyProjects"].value)
+                                            let ds = Number(row["DigitalServiceProjects"].value)
+                                            setTimeout(() => {
+                                                appendOutput(statement(<>Found {t4g + w4g + ds} projects with <strong>{office}</strong> as partner (click for more info):</>))
+                                                t4g > 0 && appendOutput(statement(<li style={{ marginLeft: "18px", textDecoration: "underline" }}>{t4g} Tech4Germany project{t4g > 1 ? "s" : ""}</li>))
+                                                w4g > 0 && appendOutput(statement(<li style={{ marginLeft: "18px", textDecoration: "underline" }}>{w4g} Work4Germany project{w4g > 1 ? "s" : ""}</li>))
+                                                ds > 0 && appendOutput(statement(<li style={{ marginLeft: "18px", textDecoration: "underline" }}>{ds} DigitalService project{ds > 1 ? "s" : ""}</li>))
+
+                                                query = `
+PREFIX : <https://digitalservice.bund.de/kg#>
+SELECT ?name WHERE {
+  :DigitalService :hasEmployee ?person .
+  ?person :hasPreviousWorkplace ?workplace .
+  ?workplace :hasName "${office}" .
+  ?person :hasFullName ?name .
+} `
+                                                fetchSelect(query, "main", responseJson => {
+                                                    console.log("Response:", responseJson)
+                                                    let names = responseJson.results.bindings.map(row => row["name"].value)
+                                                    setTimeout(() => {
+                                                        appendOutput(statement(<>Found {names.length} employee{names.length > 1 ? "s" : ""} who worked at <strong>{office}</strong> before:</>))
+                                                        for (let name of names) {
+                                                            appendOutput(statement(<li style={{ marginLeft: "18px", textDecoration: "underline" }}>{name}</li>))
+                                                        }
+                                                        query = `
+PREFIX : <https://digitalservice.bund.de/kg#>
+SELECT ?name ?contactContext ?affinityScore WHERE { 
+  ?person :isInParty "${party}" .
+  ?person :hasFullName ?name .
+  ?person :howDoWeKnowThem ?contactContext .
+  ?person :dsAffinityScore ?affinityScore .
+} `
+                                                        fetchSelect(query, "main", responseJson => {
+                                                            console.log("Response:", responseJson)
+                                                            let rows = responseJson.results.bindings
+                                                            setTimeout(() => {
+                                                                appendOutput(statement(<>Found {rows.length} contact{names.length > 1 ? "s" : ""} who is in the <strong>{party}</strong> party:</>))
+                                                                for (let row of rows) {
+                                                                    let name = row["name"].value
+                                                                    let contactContext = row["contactContext"].value
+                                                                    let affinityScore = row["affinityScore"].value
+                                                                    appendOutput(statement(<li style={{ marginLeft: "18px" }}><span style={{ textDecoration: "underline" }}>{name}</span>, {contactContext}, DS affinity score: {affinityScore}</li>))
+                                                                }
+                                                            }, DELAY_TIME)
+                                                        })
+                                                    }, DELAY_TIME)
+                                                })
+                                            }, DELAY_TIME)
+                                        })
                                     }, DELAY_TIME)
                                 }, DELAY_TIME)
                             }, DELAY_TIME)
@@ -93,6 +170,7 @@ function Experimental() {
         const bindingsStream = await sparql.fetchBindings("https://query.wikidata.org/sparql", wikidataQuery)
         bindingsStream.on("variables", vars => {})
         bindingsStream.on("data", data => {
+            console.log("Wikidata response:", data)
             let office = data["officeLabel"].value
             let party = data["partyLabel"].value
             callback(office, party)
